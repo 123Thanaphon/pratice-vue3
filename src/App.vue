a29d200d4d3219ca18edbe90a6271cadf15bd206
123thanaphon <81138308+123Thanaphon@users.noreply.github.com>
2023-01-25 17:48:39 +0700

    add option api state

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+export default {
+  data() {
+    return {
+      count: 1,
+      name: 'Tong',
+    }
+  },
+  computed: {
+    computedPuseNumber() {
+      return this.count + 1;
+    },
+  },
+  methods: {
+    methodsPlusNumber() {
+      return this.computedPuseNumber + 5;
+    },
+    changeName() {
+      this.name = "Thanaphon";
+    },
+  },
+  watch: {
+    name: {
+      handler(val, oldVal) {
+        console.log(oldVal + "Name updated to " + val);
+      },
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <p>
+      computed: {{ computedPuseNumber }}
+    </p>
+    <p>
+      methods: {{ methodsPlusNumber() }}
+      Why when I use methods,I must use ()
+    </p>
+    <p>Name: {{ name }}</p>
+    <button @click="changeName()">Click to change name</button>
+  </div>
 </template>
 
 <style scoped>
