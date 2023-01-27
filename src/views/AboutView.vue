25a5887e1eaf8194b9325959c5a0f12a45f4458d
123thanaphon <81138308+123Thanaphon@users.noreply.github.com>
2023-01-27 16:30:21 +0700

    lazy-load

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,25 +1,30 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import BaseBlack from '../components/BaseBlack.vue';
+import { ref, defineAsyncComponent } from 'vue'
+// import BaseBlack from '../components/BaseBlack.vue';
 import BaseRed from '../components/BaseRed.vue';
 
-const currentComponent = ref(BaseBlack);
+// const currentComponent = ref(BaseBlack);
 const href = false;
+
+const BaseBlack = defineAsyncComponent(() =>
+  import('../components/BaseBlack.vue')
+)
 </script>
 <template>
-  <div class="about">
-    <button @click="currentComponent = BaseRed">Switch component</button>
-    <component :is="currentComponent"></component>
+<div class="about">
+    <!-- <button @click="currentComponent = BaseRed">Switch component</button> -->
+    <!-- <component :is="currentComponent"></component> -->
+    <BaseBlack />
     <component :is="href ? 'a' : 'span'">Link</component>
-  </div>
+</div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
+.about {
     min-height: 100vh;
     display: flex;
     align-items: center;
-  }
+}
 }
 </style>
