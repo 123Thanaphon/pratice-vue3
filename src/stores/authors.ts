ab58f3e5bbea2fb88b2ce55a3ebbc3a71a22364c
123thanaphon <81138308+123Thanaphon@users.noreply.github.com>
2023-02-01 21:38:05 +0700

    remove no use code

--- src/stores/authors.ts
+++ src/stores/authors.ts
@@ -25,27 +25,5 @@ export const useAuthorsList = defineStore('authorList', () => {
         return (authorId) => state.authors.find((authorItem) => authorItem.id === authorId)
     });
 
-    // state: () => {
-    //     return {
-    //         authors: [] as AuthorInfo[],
-    //     }
-    // },
-
-    // actions: {
-    //     async fetchAuthors() {
-    //         const response = await fetch('https://mocki.io/v1/ee9fa656-9459-4bb6-92b6-f5bc008ab36c');
-    //         const data = await response.json()
-    //         return data;
-    //     },
-    //     setAuthorsList(data) {
-    //         this.authors = data;
-    //     }
-    // },
-
-    // getters: {
-    //     getOwnPost: (state) => {
-    //         return (authorId) => state.authors.find((authorItem) => authorItem.id === authorId)
-    //     },
-    // }
     return { authors, fetchAuthors, getOwnPost, setAuthorsList }
 })
