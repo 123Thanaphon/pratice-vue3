a7843a4f9bc039950621565d7efc76ea773deeea
123thanaphon <81138308+123Thanaphon@users.noreply.github.com>
2023-02-02 12:14:46 +0700

    create handle page not found

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import ForumDetail from '../components/ForumDetail.vue';
+import PageNotFound from '../components/PageNotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,22 +9,24 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
-    },
-    // {
-    //   path: "*",
-    //   component: '<p>Page Not Found</p>'
-    // },
-    {
-      path: '/maqe-test/',
-      name: 'maqeTest',
       component: () => import('../components/MaqeTest.vue'),
     },
     {
-      path: '/post/:postId/post-detail/',
-      name: 'postDetail',
-      component: ForumDetail
-    }
+      path: "/:pathMatch(.*)*",
+      component: PageNotFound,
+    },
+    {
+      path: '/maqe-test',
+      name: 'maqeTest',
+
+      children: [
+        {
+          path: ':postId/post-detail',
+          name: 'postDetail',
+          component: () => import('../components/ForumDetail.vue'),
+        }
+      ]
+    },
   ]
 })
 
