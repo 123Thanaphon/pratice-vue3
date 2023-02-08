5a874d2468c9c071b4c88ff344bc832f4dee16ef
123thanaphon <81138308+123Thanaphon@users.noreply.github.com>
2023-02-08 10:14:13 +0700

    use try catch to fetch api

--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -13,9 +13,13 @@ export const usePostsList = defineStore('postList', {
 
     actions: {
         async fetchPostList() {
-            const response = await fetch('https://mocki.io/v1/6bf4674b-da24-4452-b52a-c2d33ba40ae8');
-            const data = await response.json();
-            return data;
+            try {
+                const response = await fetch('https://mocki.io/v1/6bf4674b-da24-4452-b52a-c2d33ba40ae8');
+                const data = await response.json();
+                return data;
+            } catch(error) {
+                throw new Error('Can not fetch post list data.');
+            }
         },
         setPostsList(data: PostsInfo[]) {
             this.posts = data;
